fc4b336b7344853d1dfcfd7ca3d58aa14803a560
zn1e <moisesallen.montalbo@gmail.com>
2026-01-29 21:15:03 +1300

    style: format indentation

--- lua/plugins/file-system.lua
+++ lua/plugins/file-system.lua
@@ -1,14 +1,14 @@
 return {
-  'stevearc/oil.nvim',
-  opts = {
-	  columns = { "icon" },
-	  keymaps = {
-		  ["<esc>"] = "actions.close",
-	  },
-	  win_options = { signcolumn = "auto" },
-	  lsp_file_methods = { autosave_changes = true },
-	  view_options = { show_hidden = true }
-  },
-  dependencies = { "nvim-tree/nvim-web-devicons", opts = {} },
-  lazy = false,
+    'stevearc/oil.nvim',
+    opts = {
+      columns = { "icon" },
+      keymaps = {
+          ["<esc>"] = "actions.close",
+      },
+      win_options = { signcolumn = "auto" },
+      lsp_file_methods = { autosave_changes = true },
+      view_options = { show_hidden = true }
+    },
+    dependencies = { "nvim-tree/nvim-web-devicons", opts = {} },
+    lazy = false,
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,8 +1,12 @@
 return {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {},
+        dependencies = {
+            { "mason-org/mason.nvim",
+                opts = {},
+            },
+            "neovim/nvim-lspconfig",
+        } 
     },
 }
